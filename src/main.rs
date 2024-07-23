8f88967e466cfe2000948147ca166ee7bc8d63fe
Nedeljko Radovanovic <nradovanovic97@gmail.com>
2024-07-23 10:22:02 +0200

    Adding to game logic
    Add input error handling via match.
    Add loop that will break when certain condition meets

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,35 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("Secret number is {secret_number}");
 
-    println!("Guess the number!");
-    println!("Please input your guess.");
+    loop {
+        println!("Guess the number!");
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
 
-    println!("You have guessed: {guess}");
+        let guess: u8 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You have guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too high!"),
+            Ordering::Equal => {
+                println!("Correct!");
+                break;
+            }
+        }
+    }
 }
